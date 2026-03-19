847d4c1370b167f72e981be20507c4ec2bdab9b8
philo1698 <pilopatesameh@gmail.com>
2026-03-19 21:20:40 +0200

    start3 and initiating controller

---
+++ taskapp/src/main/java/com/project/taskapp/controllers/TaskController.java
@@ -0,0 +1,13 @@
+package com.project.taskapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+    @GetMapping("/say")
+    public String sayhellow() {
+        return "helllow";
+    }
+}
