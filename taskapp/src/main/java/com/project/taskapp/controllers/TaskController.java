f372218e124b8b0178b4cb29163972723207bffb
philo1698 <pilopatesameh@gmail.com>
2026-03-20 21:52:02 +0200

    final update of controller

--- taskapp/src/main/java/com/project/taskapp/controllers/TaskController.java
+++ taskapp/src/main/java/com/project/taskapp/controllers/TaskController.java
@@ -1,61 +1,94 @@
 package com.project.taskapp.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.taskapp.models.DAO.Task;
+import com.project.taskapp.models.DTO.IdListRequest;
+import com.project.taskapp.models.DTO.TaskUpdateRequest;
+import com.project.taskapp.service_layer.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/tasks")
 public class TaskController {
 
-    @GetMapping("/create")
-    public String createtask() {
-        return "task cereated";
+    @Autowired
+    private service taskService;
+
+    /**
+     * 1) Create task
+     */
+    @PostMapping("/create")
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
     }
 
-    @GetMapping("/delete")
-    public String deletetask(){
-        return "task deleted";
+    /**
+     * 2) Delete task
+     */
+    @PostMapping("/delete")
+    public String deleteTask(@RequestBody Long id) {
+        taskService.deleteTask(id);
+        return "Task deleted successfully";
     }
 
-    @GetMapping("/updateStatus")
-    public String updatestatus(){
-        return "status updated";
+    /**
+     * 3-6) Update fields using body
+     */
+    @PostMapping("/update/{id}")
+    public Task updateTaskFields(@PathVariable Long id,
+                                 @RequestBody TaskUpdateRequest request) {
+
+        Task task = taskService.getTaskById(id);
+        if (task == null) return null;
+
+        // Update only provided fields
+        if (request.getTitle() != null)
+            task.setTitle(request.getTitle());
+
+        if (request.getDescription() != null)
+            task.setDescription(request.getDescription());
+
+        if (request.getStatus() != null)
+            task.setStatus(request.getStatus());
+
+        if (request.getAssignedTo() != null)
+            task.setAssignedTo(request.getAssignedTo());
+
+        return taskService.updateTask(id, task);
     }
 
-    @GetMapping("/updateDescription")
-    public String updatedescription(){
-        return "description updated";
+    /**
+     * 7) Update whole task (except ID)
+     */
+    @PostMapping("/update-full/{id}")
+    public Task updateFullTask(@PathVariable Long id,
+                               @RequestBody Task task) {
+        return taskService.updateTask(id, task);
     }
 
-    @GetMapping("/updateTitle")
-    public String updatetitle(){
-        return "Title updated";
+    /**
+     * 8) Get task by ID
+     */
+    @PostMapping("/get")
+    public Task getTask(@RequestBody Long id) {
+        return taskService.getTaskById(id);
     }
 
-    @GetMapping("/updateAssignedTo")
-    public String updateassignedto(){
-        return "assigned to updated";
+    /**
+     * 9) Get all tasks
+     */
+    @GetMapping("/all")
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
     }
 
-    @GetMapping("/updateTask")
-    public String updatetask(){
-        return "task updated";
+    /**
+     * 10) Get tasks by list of IDs
+     */
+    @PostMapping("/list")
+    public List<Task> getTasksByIds(@RequestBody IdListRequest request) {
+        return taskService.getTasksByIds(request.getIds());
     }
-
-    @GetMapping("/readTask")
-    public String readtask(){
-        return "task data";
-    }
-
-    @GetMapping("/readAllTasks")
-    public String readalltask(){
-        return "all tasks data";
-    }
-
-    @GetMapping("/readTaskById")
-    public String readtaskbyid(){
-        return "needed tasks data";
-    }
-
 }
