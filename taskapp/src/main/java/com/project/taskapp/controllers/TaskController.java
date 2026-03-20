75b059abeed5aee9905fb9d6adae1f90cfc4684a
philo1698 <pilopatesameh@gmail.com>
2026-03-20 13:50:22 +0200

    adding scratch endpoints

--- taskapp/src/main/java/com/project/taskapp/controllers/TaskController.java
+++ taskapp/src/main/java/com/project/taskapp/controllers/TaskController.java
@@ -1,13 +1,51 @@
 package com.project.taskapp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/task")
 public class TaskController {
 
-    @GetMapping("/say")
-    public String sayhellow() {
-        return "hellow";
+    @GetMapping("/create")
+    public String createtask() {
+        return "task cereated";
     }
+
+    @GetMapping("/delete")
+    public String deletetask(){
+        return "task deleted";
+    }
+
+    @GetMapping("/updateStatus")
+    public String updatestatus(){
+        return "status updated";
+    }
+
+    @GetMapping("/updateDescription")
+    public String updatedescription(){
+        return "description updated";
+    }
+
+    @GetMapping("/updateTitle")
+    public String updatetitle(){
+        return "Title updated";
+    }
+
+    @GetMapping("/updateAssignedTo")
+    public String updateassignedto(){
+        return "assigned to updated";
+    }
+
+    @GetMapping("/updateTask")
+    public String updatetask(){
+        return "task updated";
+    }
+
+    @GetMapping("/readTask")
+    public String readtask(){
+        return "task data";
+    }
+
 }
