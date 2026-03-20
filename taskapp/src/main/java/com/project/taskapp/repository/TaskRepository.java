c124ca072db3bcf9c236191e73e95170c98ca8a7
philo1698 <pilopatesameh@gmail.com>
2026-03-20 21:50:03 +0200

    adding reposiyory

---
+++ taskapp/src/main/java/com/project/taskapp/repository/TaskRepository.java
@@ -0,0 +1,32 @@
+package com.project.taskapp.repository;
+
+import com.project.taskapp.models.DAO.Status;
+import com.project.taskapp.models.DAO.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Repository interface for Task entity
+ */
+@Repository // Marks this as Spring repository bean
+public interface TaskRepository extends JpaRepository<Task, Long> { //long is id data type (here id is called "id" and its data type is long
+
+    /**
+     * Find tasks by assigned person
+     * Spring automatically generates query
+     */
+    List<Task> findByAssignedTo(String assignedTo);
+
+    /**
+     * Find tasks by status
+     */
+    List<Task> findByStatus(Status status);
+
+    /**
+     * Find tasks by list of IDs
+     * (Used for your requirement #10)
+     */
+    List<Task> findByIdIn(List<Long> ids);
+}
